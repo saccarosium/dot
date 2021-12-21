eeb43fb8c3914e2a33797b72ef6e6a0c48141d48
saccarosium <sacca@macbook.local>
2021-12-21 21:43:03 +0100

    Transition to use-package

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eglot-ignored-server-capabilities
-   '(:hoverProvider :documentRangeFormattingProvider :documentOnTypeFormattingProvider)))
+ '(warning-suppress-types '((comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eglot-highlight-symbol-face ((t (:inherit bold)))))
+ )
--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,6 @@
 
 ;;; STARTUP PERFORMANCE
 
-    (setq gc-cons-threshold 100000000)
     (setq file-name-handler-alist nil)
     (add-hook 'emacs-startup-hook
             (lambda ()
@@ -17,6 +16,10 @@
     (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
     (require 'custom-functions)
 
+    ;; Silence compiler warnings as they can be pretty disruptive
+    ;;(setq comp-async-report-warnings-errors nil)
+    ;;(setq package-native-compile nil)
+
 ;;; BOOTSTRAP FOR STRAIGHT.EL
 
     (let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -30,30 +33,41 @@
         (eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
     (unless (bound-and-true-p package--initialized)
-    (setq package-enable-at-startup nil)) ; To prevent initializing twice.
+        (setq package-enable-at-startup nil)) ; To prevent initializing twice.
+
+    (setq straight-use-package-by-default t)
+    (straight-use-package 'use-package)
+    (setq use-package-verbose t)
+
+;;; BETTER GARBAGE COLLECTION
+
+    (use-package gcmh
+        :config
+        (gcmh-mode 1))
 
 ;;; DEFINING XDG DIRECTORIES
 
     ;; Make PATH on OSX work correctly
-    (straight-use-package 'exec-path-from-shell)
-    (exec-path-from-shell-initialize)
-    ;;; Pull in other enviroment variables
-    (exec-path-from-shell-copy-envs '("XDG_CONFIG_HOME" "XDG_DATA_HOME" "XDG_CACHE_HOME"))
-    (require 'osx)
-    (defvar xdg-data-home-directory (getenv "XDG_DATA_HOME"))
-    (defvar xdg-cache-home-directory (getenv "XDG_CACHE_HOME"))
+    (use-package exec-path-from-shell
+        :config
+        ;;; Pull in other enviroment variables
+        (exec-path-from-shell-initialize)
+        (exec-path-from-shell-copy-envs '("XDG_CONFIG_HOME" "XDG_DATA_HOME" "XDG_CACHE_HOME")))
+
+        (require 'osx)
+        (defvar xdg-data-home-directory (getenv "XDG_DATA_HOME"))
+        (defvar xdg-cache-home-directory (getenv "XDG_CACHE_HOME"))
 
 ;;; KEEP EMACS FOLDER CLEAN
 
-    (straight-use-package 'no-littering)
+    (use-package no-littering
+        :init
         ;;; Defining XDG_DATA_HOME $XDG_CACHE_HOME
         (setq no-littering-etc-directory
             (expand-file-name (convert-standard-filename "emacs/") xdg-cache-home-directory))
         (setq no-littering-var-directory
-            (expand-file-name (convert-standard-filename "emacs/") xdg-data-home-directory))
+            (expand-file-name (convert-standard-filename "emacs/") xdg-data-home-directory)))
 
-    ;; eww url history
-    (setq url-history-file (no-littering-expand-etc-file-name "eww/history"))
     ;; eshell history and lastdir
     (setq eshell-directory-name (no-littering-expand-etc-file-name "eshell/"))
     ;; Emacs autosave files ( #init.el# )
@@ -67,6 +81,40 @@
     (setq make-backup-file nil)
     ;; Use file for insering custom varables other that init.el
     (setq custom-file (locate-user-emacs-file "custom.el"))
+
+;;; KEYBINDS
+
+;;;; Evil Mode
+    (use-package evil
+        :init
+        (setq evil-want-integration t)
+        (setq evil-search-module 'evil-search)
+        (setq evil-undo-system 'undo-redo)
+        (setq evil-want-keybinding nil)
+        :config
+        (evil-mode 1)
+        (define-key evil-normal-state-map (kbd "C-w f") 'find-file-other-window)
+        ;; Leader Key
+        (evil-set-leader 'normal (kbd "SPC"))
+        (evil-define-key 'normal 'global (kbd "<leader>fe") #'ls/open-config)
+        (evil-define-key 'normal 'global (kbd "<leader>fn") #'deft-find-file))
+
+    ;; Global evil keybinds
+    (use-package evil-collection 
+        :after evil
+	:init
+        (evil-collection-init))
+
+;;;; Which Key
+    (use-package which-key
+        :defer t
+        :init (which-key-mode)
+        :diminish which-key-mode
+        :config
+        (setq which-key-idle-delay 1.0))
+
+;;;; General keybinds
+    (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;;; UI
     
@@ -114,11 +162,11 @@
                         :height 160
                         :weight 'regular)
 
-
     ;; More integrated themes
-    (straight-use-package 'doom-themes)
+    (use-package doom-themes
+        :config
         (load-theme 'doom-dracula t)
-        (setq doom-dracula-colorful-headers t)
+        (setq doom-dracula-colorful-headers t))
 
     (defun ls/colors ()
         (set-background-color "#191622")
@@ -136,13 +184,14 @@
         (ls/colors))
 
 ;;;; Mod-line
-    (straight-use-package 'doom-modeline)
-        (doom-modeline-mode 1)
-        (setq doom-modeline-buffer-file-name-style 'buffer-name)
-        (setq doom-modeline-major-mode-color-icon nil)
-        (setq doom-modeline-minor-modes nil)
-        (setq doom-modeline-lsp nil)
-        (setq doom-modeline-buffer-encoding nil)
+    (use-package doom-modeline
+        :hook (after-init . doom-modeline-init)
+        :custom
+        (doom-modeline-major-mode-color-icon nil)
+        (doom-modeline-minor-modes nil)
+        (doom-modeline-lsp nil)
+        (doom-modeline-buffer-file-name-style 'buffer-name)
+        (doom-modeline-buffer-encoding nil))
 
 ;;;; Relative number like in Vim
     (straight-use-package 'linum-relative)
@@ -150,164 +199,169 @@
         (setq linum-relative-backend 'display-line-numbers-mode)
 
 ;;;; Make some windows a popup
-    (straight-use-package 'popper)
-    (setq popper-reference-buffers
-            '(xref-mode
-            "\\*Occur\\*"
-            ".*.png"
-            "\\*Flymake\\ diagnostics.*"
-            xref--xref-buffer-mode))
-    (global-set-key (kbd "C-,") 'popper-toggle-latest)
-    (global-set-key (kbd "C-S-;") 'popper-cycle)
-    (popper-mode +1)
-    (popper-echo-mode +1)
-
-;;; KEYBINDS
-
-;;;; Evil Mode
-    (straight-use-package 'evil)
-        (setq evil-search-module 'evil-search)
-        (setq evil-undo-system 'undo-redo)
-        (setq evil-want-keybinding nil)
-        (evil-mode 1)
-        (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-        (define-key evil-normal-state-map (kbd "C-w f") 'find-file-other-window)
-        ;; Leader Key
-        (evil-set-leader 'normal (kbd "SPC"))
-        (evil-define-key 'normal 'global (kbd "<leader>fe") #'ls/open-config)
-        (evil-define-key 'normal 'global (kbd "<leader>fn") #'deft-find-file)
-        (evil-define-key 'normal 'global (kbd "TAB") 'outline-cycle)
-
-    ;; Global evil keybinds
-    (straight-use-package 'evil-collection)
-        (setq evil-want-integration t)
-        (evil-collection-init)
-
-;;;; Which Key
-    (straight-use-package 'which-key)
-        (setq which-key-idle-delay 1.0)
-        (which-key-mode)
-
-;;;; General keybinds
-    (global-set-key (kbd "C-x C-b") 'ibuffer)
+    (use-package popper
+        :bind (("C-,"   . popper-toggle-latest)
+               ("C-;"   . popper-cycle))
+        :init
+        (setq popper-reference-buffers
+                '(xref-mode
+                "\\*Occur\\*"
+                ".*.png"
+                "\\*Flymake\\ diagnostics.*"
+                xref--xref-buffer-mode))
+        (popper-mode +1)
+        (popper-echo-mode +1))                ; For echo area hints
 
 ;;; UTILITY
 
-    (straight-use-package 'restart-emacs)
+    (use-package restart-emacs)
 
 ;;; BUILTINGS PACKAGES
 
-    (straight-use-package '(eww :type built-in))
-        (global-set-key (kbd "C-x ?") 'eww)
+    (use-package eww
+        :straight (:type built-in)
+        :defer t
+        :bind ("C-x ?" . eww)
+        :config
         (setq eww-desktop-remove-duplicates t)
         (setq eww-header-line-format nil)
         (setq eww-search-prefix "https://duckduckgo.com/html/?q=")
         (setq url-cookie-delete-cookies nil)
-        (setq url-cookie-confirmation nil)
+        ;; eww url history
+        (setq url-history-file (no-littering-expand-etc-file-name "eww/history"))
+        (setq url-cookie-confirmation nil))
 
-    (straight-use-package '(dired :type built-in))
+    (use-package dired
+        :straight (:type built-in)
+        :config
         (put 'dired-find-alternate-file 'disabled nil)
         (evil-define-key 'normal dired-mode-map "h" 'dired-up-directory)
         (evil-define-key 'normal dired-mode-map "l" 'dired-find-alternate-file)
         (evil-define-key 'normal dired-mode-map " " 'dired-mark)
         (evil-define-key 'normal dired-mode-map "q" 'kill-this-buffer)
         (set-face-attribute 'dired-directory nil :foreground "#bd93f9")
-        (set-face-attribute 'dired-symlink nil :foreground "#50fa7b")
+        (set-face-attribute 'dired-symlink nil :foreground "#50fa7b"))
 
-    (straight-use-package '(project :type built-in))
-        (global-set-key (kbd "C-x p s") 'project-switch-project)
+    (use-package project
+        :straight (:type built-in)
+        :bind ("C-x p s" . project-switch-project))
 
-    (straight-use-package '(outline :type built-in))
-        (add-hook 'prog-mode-hook 'outline-minor-mode)
+    (use-package outline
+        :straight (:type built-in)
+        :hook (prog-mode . outline-minor-mode)
+        :config
+        (evil-define-key 'normal 'global (kbd "TAB") 'outline-cycle))
+
 
 ;;; COMPLETION FRAMWORK
 
 ;;;; Vertico for the vertical UI
-    (straight-use-package
-    '(vertico :host github :repo "minad/vertico" :branch "main"))
-        (setq vertico-cycle t)
-        (vertico-mode)
+    (use-package vertico
+        :straight (:host github :repo "minad/vertico" :branch "main")
+        :custom
+        (vertico-cycle t)
+        :init
+        (vertico-mode))
 
 ;;;; Orderless for the sweat fuzzy-like maching
-    (straight-use-package 'orderless)
-        (setq completion-styles '(orderless))
+    (use-package orderless
+        :init (setq completion-styles '(orderless)))
 
 ;;;; Marginalia for extra info on the margins
-    (straight-use-package 'marginalia)
-        (marginalia-mode)
+    (use-package marginalia
+        :after vertico
+        :config
+        (marginalia-mode))
 
 ;;; WRITING/NOTES
 
 ;;;; Notational Velocity for emacs
-    (straight-use-package 'deft)
+    (use-package deft
+        :commands deft-find-file
+        :config
         (setq deft-directory "~/Documents/nextcloud/notes/"
-            deft-extensions '("md" "org")
-            deft-recursive t)
+              deft-extensions '("md" "org")
+              deft-recursive t))
 
 ;;;; Markdown support in emacs
-    (straight-use-package 'markdown-mode)
-        (setq markdown-command "Pandoc")
+    (use-package markdown-mode
+        :mode "\\.md\\'"
+        :config
+        (setq markdown-command "Pandoc"))
 
 ;;;; Org Mode
-    (straight-use-package '(org :type built-in))
+    (use-package org
+        :straight (:type built-in)
+        :defer t
+        :config
         (setq org-ellipsis " ▾")
         (setq org-src-fontify-natively t)
         (setq org-fontify-quote-and-verse-blocks t)
         (setq org-src-tab-acts-natively t)
         (setq org-edit-src-content-indentation 2)
         (setq org-src-preserve-indentation nil)
-        (setq org-cycle-separator-lines 2)
+        (setq org-cycle-separator-lines 2))
 
 ;;;; Center the writing area
-    (straight-use-package 'olivetti)
-        (add-hook 'markdown-mode-hook 'olivetti-mode)
-        (add-hook 'org-mode-hook 'olivetti-mode)
-        (add-hook 'eww-mode-hook 'olivetti-mode)
+    (use-package olivetti
+        :hook ((markdown-mode . olivetti-mode)
+               (org-mode . olivetti-mode)
+               (eww-mode . olivetti-mode)))
 
 ;;; PROGRAMMING
 
 ;;;; Sweat Tree sitter
-    (straight-use-package 'tree-sitter-langs)
-    (straight-use-package 'tree-sitter)
-        (add-hook 'tree-sitter-after-on-hook 'tree-sitter-hl-mode)
+    (use-package tree-sitter-langs
+        :after tree-sitter)
+    (use-package tree-sitter
+        :config
         (global-tree-sitter-mode)
+        (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 ;;;; Eglot for lsp support
-    (straight-use-package 'eglot)
+    (use-package eglot
+        :hook ((c-mode . eglot-ensure)
+               (c++-mode . eglot-ensure))
+        :init
         (setq eglot-server-programs '((c-mode . ("clangd"))))
         (setq eglot-ignored-server-capabilities '(:hoverProvider :documentRangeFormattingProvider :documentOnTypeFormattingProvider))
-        (add-hook 'c-mode-hook 'eglot-ensure)
-        (add-hook 'c++-mode-hook 'eglot-ensure)
+        :config
         (evil-define-key 'normal eglot-mode-map (kbd "g R") 'eglot-rename)
         (evil-define-key 'normal eglot-mode-map (kbd "g d") 'eglot-find-declaration)
         (evil-define-key 'normal eglot-mode-map (kbd "g D") 'flymake-show-buffer-diagnostics)
         (evil-define-key 'normal eglot-mode-map (kbd "g r") 'xref-find-references)
-        (evil-define-key 'normal eglot-mode-map (kbd "g h") 'eldoc)
+        (evil-define-key 'normal eglot-mode-map (kbd "g h") 'eldoc))
 
 ;;;; Completion w/company
-    (straight-use-package 'company)
-        (add-hook 'after-init-hook 'global-company-mode)
+    (use-package company
+        :hook (after-init-hook . global-company-mode)
+        :config
         (setq company-backends '(company-files company-capf))
         (setq company-auto-commit-chars nil)
         (setq company-minimum-prefix-length 1)
         (setq company-idle-delay 0.0)
         (setq company-auto-commit t)
-        (setq company-icon-size '(auto-scale . 15))
+        (setq company-icon-size '(auto-scale . 15)))
 
 ;;;; Companion for company
-    (straight-use-package 'company-box)
-        (add-hook 'company-mode 'company-box-mode)
+    (use-package company-box
+        :hook (company-mode . company-box-mode))
 
 ;;;; VC Mode
-    (straight-use-package '(vc :type built-in))
+    (use-package vc
+        :straight (:type built-in)
+        :bind ("C-x v d" . vc-dir-root)
+        :config
         (setq auto-revert-check-vc-info t)
-        (setq vc-follow-symlinks t)
-        (global-set-key (kbd "C-x v d") 'vc-dir-root)
+        (setq vc-follow-symlinks t))
 
 ;;;; The best git interface
-    (straight-use-package 'magit)
+    (use-package magit
+        :commands (magit-status magit-get-current-branch))
 
 ;;;; Autosave
-    (straight-use-package 'super-save)
+    (use-package super-save
+        :defer 1
+        :config
         (super-save-mode +1)
-        (setq super-save-auto-save-when-idle t)
+        (setq super-save-auto-save-when-idle t))
